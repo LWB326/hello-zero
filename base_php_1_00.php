d02122cca26c6028b1b148a7c0e67f0302d84f67
Michel Fleury <my.fleury@gmail.com>
2017-03-28 17:45:00 +0200

    premier commit 27 mars 2017

---
+++ base_php_1_00.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">  
+    
+    <head>
+        <title > base_php_1_00.php</title >
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+    </head>
+
+    <body>
+       <!-- Affichage de  2 lignes de test -->
+        
+        <p>Cette ligne test est écrite en html (dur)
+        </p>
+        <p> 
+            <?php
+                echo "Cette ligne test a été écrite en php";
+            ?>    
+        </p>          
+       <!-- Code à tester à insérer ci-dessous -->
+        <p>  
+        </p>
+        
+        
+    </body>
+</html>
